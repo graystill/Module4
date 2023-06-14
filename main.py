0192b549d7cd1381d3573eb485f13dd84dad5bc4
A_A_D <pilcuktatana863@gmail.com>
2023-06-14 12:12:32 +0300

    #[::-1] - переворачивает строку и если реверснутая строка равна исходной, то возвращает True, иначе - False

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# def string_counter(s):
-#     syms_counter = {}
-#     for sym in s:
-#         syms_counter[sym] = syms_counter.get(sym, 0) + 1
-#
-#         for sym, count in syms_counter.items():
-#             print(sym, count)
-#
-#
-# string_counter('aaaabbsbdbd')
+def string_counter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+        for sym, count in syms_counter.items():
+            print(sym, count)
+
+
+string_counter('aaaabbsbdbd')
 
 # ДЗ (урок 1, модуль 4)
 
